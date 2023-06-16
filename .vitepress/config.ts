45c2ecdc275026eff22ed21f2054ff3c6d38d68f
Kira <6999959@gmail.com>
2023-06-16 22:20:03 +0800

    feat: 增加github在线访问

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,36 +3,35 @@ import { defineConfig } from 'vitepress'
 const relative = path => `/views${path}`
 export default defineConfig({
   title: '我的文档',
-  base:'./',
+  base: './',
   description: 'A VitePress Site',
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/views/examples/markdown-example' }
     ],
     sidebar: [
       {
         text: 'Examples',
         items: [
-          { text: 'Markdown Examples', link: relative('/examples/markdown-examples') },
-          { text: 'Runtime API Examples', link: relative('/examples/api-examples') }
+          { text: 'Markdown Examples', link: relative('/views/examples/markdown-examples') },
+          { text: 'Runtime API Examples', link: relative('/views/examples/api-examples') }
         ]
       },
       {
         text: 'Typescript',
         items: [
-          { text: 'typeof', link: relative('/typescript/typeof') },
-          { text: 'keyof', link: relative('/typescript/keyof') },
-          { text: 'as const', link: relative('/typescript/as const') },
-          { text: 'extends', link: relative('/typescript/extends') },
-          { text: 'infer', link: relative('/typescript/infer') },
-          { text: 'in', link: relative('/typescript/in') },
-          { text: '字符串索引（对象）', link: relative('/typescript/字符串索引（对象）') },
-          { text: '数字索引（数组）', link: relative('/typescript/数字索引（数组）') }
+          { text: 'typeof', link: relative('/views/typescript/typeof') },
+          { text: 'keyof', link: relative('/views/typescript/keyof') },
+          { text: 'as const', link: relative('/views/typescript/as const') },
+          { text: 'extends', link: relative('/views/typescript/extends') },
+          { text: 'infer', link: relative('/views/typescript/infer') },
+          { text: 'in', link: relative('/views/typescript/in') },
+          { text: '字符串索引（对象）', link: relative('/views/typescript/字符串索引（对象）') },
+          { text: '数字索引（数组）', link: relative('/views/typescript/数字索引（数组）') }
         ]
       }
     ],
-
     socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }]
   }
 })
